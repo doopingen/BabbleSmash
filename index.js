c7c5c40943bcbc96823192ece6af243b6bd3ad92
doopingen <clay@darland.info>
2019-10-22 15:43:15 -0700

    index.js setup changes

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 //Initialize Express
 const express = require('express');
-const path = require('path');  
+const path = require('path');
 
-//Setup Express for use
+//Initialize Authentication
+const ejsLayouts = require('express-ejs-layouts');
+const session = require('express-session');
+const passport = require('./config/ppConfig');
+const flash = require('connect-flash');
+const isLoggedIn = require('./middleware/isLoggedIn');
+const helmet = require('helmet');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const RateLimit = require('express-rate-limit');
+
+//Set Database models
+const db = require('./models');
+
+//Setup Express to use it as a middleware
 const app = express();
 
 //Set default view engine to Pug and set views directory
@@ -11,6 +24,9 @@ app.set('view engine', 'pug');
 
 //Setup Express
 app.use(express.static('public'));
+app.use(require('morgan')('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
 
 //Setup Production routes 
 app.use('/', require('./routes/prodRoutes'));
