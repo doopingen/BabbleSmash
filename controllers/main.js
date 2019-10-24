6302cf87eba1f9d8b436f3e2d49cee3a5702ac8c
doopingen <clay@darland.info>
2019-10-24 01:00:43 -0700

    Making changes to profile layout. Unhappy with presentation

--- controllers/main.js
+++ controllers/main.js
@@ -55,7 +55,37 @@ router.get('/profile', isLoggedIn, function(req, res) {
   })
 });
 
-//POST - 
+//POST - Save Route
+router.post('/save', function(req, res) {
+  db.user.findByPk(req.user.id).then(function(user) {
+    user.createArticle({
+      text: req.body.sentiment,
+      summary: req.body.summary,
+      url: req.body.url,
+    })
+  })
+ .then(function(article) {
+    console.log(article);
+    res.redirect('profile')
+  })
+});
+
+// post.getAuthor().then(function(author) {
+//   post.getComments().then(function(comments) {
+//       res.render('posts/show', {post, author, comments})
+
+// db.post.findByPk(postId)
+//     .then(function(post) {
+//         console.log(`Post: ${post}`)
+//         db.tag.findOrCreate({
+//             where:{
+//                 name: req.body.name
+//             }
+//         }).then(function([tag, created]) {
+//             console.log(`Tag: ${tag}`)
+//             post.addTag(tag).then(function(data) {
+//                 res.redirect(`/posts/${postId}`)
+//             })
 
 //POST - Login route
 router.post('/login', passport.authenticate('local', {
