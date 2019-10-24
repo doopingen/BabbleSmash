71efc6e24dc8d79cb4469084ef4d1b69c429c9f1
doopingen <clay@darland.info>
2019-10-24 01:52:45 -0700

    Made changes to profile layout and tested delete capabilities

--- controllers/main.js
+++ controllers/main.js
@@ -70,23 +70,6 @@ router.post('/save', function(req, res) {
   })
 });
 
-// post.getAuthor().then(function(author) {
-//   post.getComments().then(function(comments) {
-//       res.render('posts/show', {post, author, comments})
-
-// db.post.findByPk(postId)
-//     .then(function(post) {
-//         console.log(`Post: ${post}`)
-//         db.tag.findOrCreate({
-//             where:{
-//                 name: req.body.name
-//             }
-//         }).then(function([tag, created]) {
-//             console.log(`Tag: ${tag}`)
-//             post.addTag(tag).then(function(data) {
-//                 res.redirect(`/posts/${postId}`)
-//             })
-
 //POST - Login route
 router.post('/login', passport.authenticate('local', {
   successRedirect: 'profile',
